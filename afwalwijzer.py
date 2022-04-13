9cff75a201d0c288d6fdc7802d6a21b67000dda3
Leo van der Meulen <leo.vander.meulen@gmail.com>
2022-04-13 17:57:46 +0200

    Update afwalwijzer.py

--- afwalwijzer.py
+++ afwalwijzer.py
@@ -1,5 +1,5 @@
-import requests
 import json
+import requests
 
 class Afvalwijzer:
     
@@ -17,40 +17,47 @@ class Afvalwijzer:
         self.ophaaldata = None
 
     def resolve(self):
-        url = 'https://inzamelwijzer.prezero.nl/adressen/'+ self.postcode + ':' + str(self.huisnr)
-        response = requests.get(url)
-        data = response.json()
-        self.bagid = response.json()[0]['bagid']
-        return self.bagid
+        try:
+            url = 'https://inzamelwijzer.prezero.nl/adressen/'+ self.postcode + ':' + str(self.huisnr)
+            response = requests.get(url)
+            data = response.json()
+            self.bagid = response.json()[0]['bagid']
+        except Exception as e:
+            print('Helaas... {}'.format(e))
+            self.bagid = None
     
     def retrieve_data(self):
         if not self.bagid:
             self.resolve()
-        url = 'https://inzamelwijzer.prezero.nl/rest/adressen/' + self.bagid + '/afvalstromen'
-        response = requests.get(url)
-        self.afvalinfo = json.loads(response.text)
-        
+        try:
+            url = 'https://inzamelwijzer.prezero.nl/rest/adressen/' + self.bagid + '/afvalstromen'
+            response = requests.get(url)
+            self.afvalinfo = json.loads(response.text)
+        except Exception as e:
+            print('Helaas... {}'.format(e))
+            self.afvalinfo = []
+
     def get_ophaaldata(self):
+        self.ophaaldata = None
         if not self.afvalinfo:
             self.retrieve_data()
-        ophaaldata = []
+        self.ophaaldata = []
         for afl in afval.afvalinfo:
             if afl['ophaaldatum']:
                 el = {'product':afl['title'].replace(' Arnhem', ''), 'datum': afl['ophaaldatum']}
-                ophaaldata.append(el)        
-        self.ophaaldata = ophaaldata
-        return ophaaldata
+                self.ophaaldata.append(el)        
     
     def __get_datum__(self, product):
         if not self.ophaaldata:
             self.get_ophaaldata()
-        for d in self.ophaaldata:
-            if d['product'] == product:
-                return d['datum']
+        if self.ophaaldata:
+            for d in self.ophaaldata:
+                if d['product'] == product:
+                    return d['datum']
         return None
             
     def get_volgende_groen(self):
-        return self.__get_datum__('Groente-, fruit en tuinafval')
+        return self.__get_datum__('Groente-, fruit en tinafval')
     
     def get_volgende_plastic(self):
         return self.__get_datum__('Plastic, blik & drinkpakken')
